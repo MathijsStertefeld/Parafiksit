a9093c9d74a3ebbd71c7078c12dde46068cf4241
Ruud Lenders <rmwlenders@gmail.com>
2013-05-29 14:07:42 +0200

    Eerste opzet van alle Maven projecten

---
+++ ComputerBroker/src/main/java/com/marbl/parafiksit/computerbroker/App.java
@@ -0,0 +1,13 @@
+package com.marbl.parafiksit.computerbroker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MagazijnApp/src/main/java/com/marbl/parafiksit/magazijnapp/App.java
@@ -0,0 +1,13 @@
+package com.marbl.parafiksit.magazijnapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
