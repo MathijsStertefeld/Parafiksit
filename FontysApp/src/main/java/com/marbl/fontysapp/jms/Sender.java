621603402b0e49744cf6e4a31743fff3e254346e
Leslie Aerts <neohoodman@gmail.com>
2013-06-19 15:20:03 +0200

    client domain classes

---
+++ FontysApp/src/main/java/com/marbl/fontysapp/jms/Sender.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.marbl.fontysapp.jms;
+
+import com.marbl.fontysapp.domain.Order;
+
+/**
+ *
+ * @author Leslie Aerts
+ */
+public class Sender
+{
+    public Sender(Order o)
+    {
+        
+    }
+}
