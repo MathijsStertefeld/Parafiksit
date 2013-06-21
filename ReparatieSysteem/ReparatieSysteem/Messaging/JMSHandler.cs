7118cd7b69a7d7bc207a168455389b9855f882ab
Mathijs Stertefeld <m.stertefeld@student.fontys.nl>
2013-06-21 15:15:43 +0200

    JMS in C#

---
+++ ReparatieSysteem/ReparatieSysteem/Messaging/JMSHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Apache.NMS.ActiveMQ;
+using Spring.Messaging.Nms;
+using Spring.Messaging.Nms.Listener;
+using Spring.Messaging.Nms.Core;
+using JMSTest;
+
+namespace ReparatieSysteem.Messaging
+{
+    class JMSHandler
+    {
+        private const string URI = "tcp://localhost:61616";
+        private const string DESTINATION = "test.queue";
+        private NmsTemplate template;
+
+        public JMSHandler()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory(URI);
+            template = new NmsTemplate(connectionFactory);
+
+            using (SimpleMessageListenerContainer listenerContainer = new SimpleMessageListenerContainer())
+            {
+                listenerContainer.ConnectionFactory = connectionFactory;
+                listenerContainer.DestinationName = DESTINATION;
+                listenerContainer.MessageListener = new Listener();
+                listenerContainer.AfterPropertiesSet();
+            }
+        }
+
+        public void Send(String message)
+        {
+            try
+            {
+                template.ConvertAndSend(DESTINATION, message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+    }
+}
---
+++ ReparatieSysteem/ReparatieSysteem/Messaging/Listener.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Spring.Messaging.Nms;
+using Apache.NMS;
+using Spring.Messaging.Nms.Core;
+
+namespace JMSTest
+{
+    class Listener : IMessageListener
+    {
+        public void OnMessage(IMessage message)
+        {
+            ITextMessage textMessage = message as ITextMessage;
+            Console.WriteLine(textMessage.Text);
+        }
+    }
+}
