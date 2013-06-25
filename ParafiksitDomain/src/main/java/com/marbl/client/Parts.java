d9c79166ded3dee99f9d398086bbb5b69c7018c3
Leslie Aerts <neohoodman@gmail.com>
2013-06-25 10:25:27 +0200

    Static parts

---
+++ ParafiksitDomain/src/main/java/com/marbl/client/Parts.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.marbl.client;
+
+/**
+ *
+ * @author Leslie Aerts
+ */
+public class Parts
+{
+    public static final String HARD_DISK = "Harde schijf";
+    public static final String MONITOR = "Beeldscherm";
+    public static final String NETWORK_CABLE = "Netwerkkabel";
+    public static final String RAM = "RAM";
+}
